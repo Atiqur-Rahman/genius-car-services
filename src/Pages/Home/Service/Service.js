fee5ac706aae63b49171aff039d47a631ccb9c3c
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-31 23:12:16 +0600

    Make Services Responsive using CSS media query

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './service.css';
 
 const Service = ({ service }) => {
     const { name, price, description, img } = service;
     return (
-        <div>
-            <img src={img} alt="" />
+        <div className="service">
+            <img style={{ width: '100%', height: '400px' }} src={img} alt="" />
             <h2>{name}</h2>
+            <p>Price: {price}</p>
+            <p>
+                <small>{description}</small>
+            </p>
+            <button>Book: {name}</button>
         </div>
     );
 };
