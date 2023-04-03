4bee1125051dfffaa75d964e1f9decd1f8d8e73e
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-04-03 19:45:52 +0600

    Style social login and share social login component

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -35,13 +36,10 @@ const Login = () => {
             <h2 className="text-primary text-center">Please Login</h2>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -57,6 +55,7 @@ const Login = () => {
                     Please Register
                 </Link>
             </p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import google from '../../../images/social/google.png';
+import facebook from '../../../images/social/facebook.png';
+import github from '../../../images/social/github.png';
+
+const SocialLogin = () => {
+    return (
+        <div>
+            <div className="d-flex align-items-center">
+                <div style={{ height: '1px' }} className="bg-secondary w-50"></div>
+                <p className="mt-3 px-3">OR</p>
+                <div style={{ height: '1px' }} className="bg-secondary w-50"></div>
+            </div>
+            <div>
+                <button className="btn btn-info w-50 d-block mx-auto my-2">
+                    <img src={google} alt="" />
+                    <span className="px-2">Google Sign In</span>
+                </button>
+                <button className="btn btn-info w-50 d-block mx-auto my-2">
+                    <img src={facebook} alt="" />
+                    <span className="px-2">Facebook Sign In</span>
+                </button>
+                <button className="btn btn-info w-50 d-block mx-auto">
+                    <img src={github} alt="" />
+                    <span className="px-2">Github Sign In</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SocialLogin;
